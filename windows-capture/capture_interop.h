513e8fe1529965311bf1c76ae3246ac3689160d6
Jozsef Vass <jozsef_vass@yahoo.com>
2022-11-18 15:52:52 -0800

    process integrity level hardening by Mark

--- windows-capture/capture_interop.h
+++ windows-capture/capture_interop.h
@@ -10,6 +10,7 @@ inline winrt::Windows::Graphics::Capture::GraphicsCaptureItem CreateCaptureItemF
     auto interop_factory = activation_factory.as<IGraphicsCaptureItemInterop>();
     winrt::Windows::Graphics::Capture::GraphicsCaptureItem item = { nullptr };
     interop_factory->CreateForWindow(hwnd, winrt::guid_of<ABI::Windows::Graphics::Capture::IGraphicsCaptureItem>(), reinterpret_cast<void**>(winrt::put_abi(item)));
+    assert(item);
     OutputDebugString(item.DisplayName().c_str());
     return item;
 }
--- windows-capture/windows-capture.cpp
+++ windows-capture/windows-capture.cpp
@@ -51,12 +51,96 @@ LRESULT CALLBACK WndProc(
     return 0;
 }
 
+//
+// Taken from: https://chromium.googlesource.com/chromium/src/sandbox/+/refs/heads/main/win/src/restricted_token_utils.cc
+//
+
+DWORD GetObjectSecurityDescriptor(
+    HANDLE handle,
+    SECURITY_INFORMATION security_info,
+    std::vector<char>* security_desc_buffer,
+    PSECURITY_DESCRIPTOR* security_desc)
+{
+    DWORD last_error = 0;
+    DWORD length_needed = 0;
+    ::GetKernelObjectSecurity(handle, security_info, nullptr, 0, &length_needed);
+
+    last_error = ::GetLastError();
+    if (last_error != ERROR_INSUFFICIENT_BUFFER)
+        return last_error;
+
+    security_desc_buffer->resize(length_needed);
+    *security_desc =
+        reinterpret_cast<PSECURITY_DESCRIPTOR>(security_desc_buffer->data());
+    if (!::GetKernelObjectSecurity(handle, security_info, *security_desc,
+        length_needed, &length_needed)) {
+        return ::GetLastError();
+    }
+
+    return ERROR_SUCCESS;
+}
+
+DWORD HardenTokenIntegrityLevelPolicy(HANDLE token)
+{
+    std::vector<char> security_desc_buffer;
+    PSECURITY_DESCRIPTOR security_desc = nullptr;
+    DWORD last_error = GetObjectSecurityDescriptor(
+        token, LABEL_SECURITY_INFORMATION, &security_desc_buffer, &security_desc);
+
+    if (last_error != ERROR_SUCCESS)
+        return last_error;
+
+    PACL sacl = nullptr;
+    BOOL sacl_present = false;
+    BOOL sacl_defaulted = false;
+    if (!::GetSecurityDescriptorSacl(security_desc, &sacl_present, &sacl,
+        &sacl_defaulted)) {
+        return ::GetLastError();
+    }
+
+    for (DWORD ace_index = 0; ace_index < sacl->AceCount; ++ace_index) {
+        PSYSTEM_MANDATORY_LABEL_ACE ace;
+        if (::GetAce(sacl, ace_index, reinterpret_cast<LPVOID*>(&ace)) &&
+            ace->Header.AceType == SYSTEM_MANDATORY_LABEL_ACE_TYPE)
+        {
+            // We only need the NO_READ to reproduce this
+            ace->Mask |= SYSTEM_MANDATORY_LABEL_NO_READ_UP /* | SYSTEM_MANDATORY_LABEL_NO_EXECUTE_UP*/;
+            break;
+        }
+    }
+
+    if (!::SetKernelObjectSecurity(token, LABEL_SECURITY_INFORMATION,
+        security_desc))
+        return ::GetLastError();
+
+    return ERROR_SUCCESS;
+}
+
+DWORD HardenProcessIntegrityLevelPolicy() {
+    HANDLE token_handle;
+    if (!::OpenProcessToken(GetCurrentProcess(), READ_CONTROL | WRITE_OWNER,
+        &token_handle))
+        return ::GetLastError();
+
+    const auto retval = HardenTokenIntegrityLevelPolicy(token_handle);
+    CloseHandle(token_handle);
+
+    return retval;
+}
+
 int CALLBACK WinMain(
     HINSTANCE instance,
     HINSTANCE previousInstance,
     LPSTR     cmdLine,
     int       cmdShow)
 {
+    // Harden the process integrity level before attempting a Windows graphics capture
+    // This results in null GraphicsCaptureItem
+    DWORD error = HardenProcessIntegrityLevelPolicy();
+    if ((error != ERROR_SUCCESS) && (error != ERROR_ACCESS_DENIED)) {
+        return false;
+    }
+
     // Init COM
     winrt::init_apartment(winrt::apartment_type::single_threaded);
 
@@ -118,7 +202,15 @@ int CALLBACK WinMain(
     auto device = CreateDirect3DDevice(dxgiDevice.get());
 #endif
 
+    // window selection
     size_t windowIndex = 0;
+    for (int i = 0; i < windows.size(); i++) {
+        if (windows[i].title().find(L"League") != std::string::npos) {
+            windowIndex = i;
+            break;
+        }
+    }
+
     std::wstringstream str;
     str << "Capturing window " << ": " << windows[windowIndex].title() << std::endl;
     OutputDebugString(str.str().c_str());
